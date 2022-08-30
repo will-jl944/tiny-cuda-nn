4bd7bf3c7c7bc5f596c1f7c44d65b7779fbb9350
will-jl944 <lujiafeng@baidu.com>
2022-08-30 14:21:23 +0800

    paddle tinycudann

---
+++ bindings/paddle/tinycudann/bindings.cpp
@@ -0,0 +1,341 @@
+/*
+ * Copyright (c) 2020-2022, NVIDIA CORPORATION.  All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *     * Redistributions of source code must retain the above copyright notice, this list of
+ *       conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice, this list of
+ *       conditions and the following disclaimer in the documentation and/or other materials
+ *       provided with the distribution.
+ *     * Neither the name of the NVIDIA CORPORATION nor the names of its contributors may be used
+ *       to endorse or promote products derived from this software without specific prior written
+ *       permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+ * FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL NVIDIA CORPORATION BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING,
+ * BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+ * STRICT LIABILITY, OR TOR (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *//*
+ */
+
+/** @file   torch_bindings.cu
+ *  @author Thomas Müller, Jacob Munkberg, Jon Hasselgren, Or Perel, NVIDIA
+ */
+
+#ifdef _MSC_VER
+#pragma warning(push, 0)
+#include <paddle/extension.h>
+#pragma warning(pop)
+#else
+#include <paddle/extension.h>
+#endif
+
+#include <c10/cuda/CUDAGuard.h>
+
+#ifdef snprintf
+#undef snprintf
+#endif
+
+#include <json/json.hpp>
+
+#include <pybind11_json/pybind11_json.hpp>
+
+#include <tiny-cuda-nn/cpp_api.h>
+
+#define STRINGIFY(x) #x
+#define STR(x) STRINGIFY(x)
+#define FILE_LINE __FILE__ ":" STR(__LINE__)
+#define CHECK_THROW(x) \
+	do { if (!(x)) throw std::runtime_error(std::string(FILE_LINE " check failed " #x)); } while(0)
+
+c10::ScalarType paddle_type(tcnn::cpp::EPrecision precision) {
+	switch (precision) {
+		case tcnn::cpp::EPrecision::Fp32: return paddle::DataType::FLOAT32;
+		case tcnn::cpp::EPrecision::Fp16: return paddle::DataType::FLOAT16;
+		default: throw std::runtime_error{"Unknown precision tcnn->paddle"};
+	}
+}
+
+void* void_data_ptr(const paddle::Tensor& tensor) {
+	switch (tensor.dtype()) {
+		case paddle::DataType::FLOAT32: return tensor.data<float>();
+		case paddle::DataType::FLOAT16: return tensor.data<paddle::float16>();
+		default: throw std::runtime_error{"Unknown precision paddle->void"};
+	}
+}
+
+#define CHECK_INPUT(x) PD_CHECK(x.is_gpu(), #x " must be a GPU Tensor.")
+
+class Module {
+public:
+	Module(tcnn::cpp::Module* module) : m_module{module} {}
+
+	std::tuple<tcnn::cpp::Context, paddle::Tensor> fwd(const paddle::Tensor& input, const paddle::Tensor& params, bool input_requires_grad, bool params_requires_grad) {
+		CHECK_INPUT(input);
+		CHECK_INPUT(params);
+
+		// Types
+		PD_CHECK(input.dtype() == paddle::DataType::FLOAT32, "Input data type must be float32");
+		CHECK_THROW(params.dtype() == c10_param_precision());
+
+		// Sizes
+		CHECK_THROW(input.shape[1] == n_input_dims());
+		CHECK_THROW(params.shape[0] == n_params());
+
+		// Device
+		auto place = input.place();
+		CHECK_THROW(device == params.place());
+
+		cudaStream_t stream = input.stream();
+
+		uint32_t batch_size = static_cast<uint32_t>(input.shape[0]);
+
+		paddle::Tensor output = paddle.empty({ batch_size, n_output_dims() }, c10_output_precision(), place);
+
+		tcnn::cpp::Context ctx;
+		if (!input_requires_grad && !params_requires_grad) {
+			m_module->inference(stream, batch_size, input.data<float>(), void_data_ptr(output), void_data_ptr(params));
+		} else {
+			ctx = m_module->forward(stream, batch_size, input.data<float>(), void_data_ptr(output), void_data_ptr(params), input_requires_grad);
+		}
+
+		return { std::move(ctx), output };
+	}
+
+	std::tuple<paddle::Tensor, paddle::Tensor> bwd(const tcnn::cpp::Context& ctx, const paddle::Tensor& input, const paddle::Tensor& params, const paddle::Tensor& output, const paddle::Tensor& dL_doutput, bool input_requires_grad, bool params_requires_grad) {
+		if (!ctx.ctx) {
+			throw std::runtime_error{"Module::bwd: called with invalid context. fwd likely (mistakenly) ran in inference mode."};
+		}
+
+		CHECK_INPUT(input);
+		CHECK_INPUT(params);
+		CHECK_INPUT(output);
+		CHECK_INPUT(dL_doutput);
+
+		// Types
+		CHECK_THROW(input.dtype() == paddle::DataType::FLOAT32);
+		CHECK_THROW(params.dtype() == c10_param_precision());
+		CHECK_THROW(output.dtype() == c10_output_precision());
+		CHECK_THROW(dL_doutput.dtype() == c10_output_precision());
+
+		// Sizes
+		CHECK_THROW(input.shape[1] == n_input_dims());
+		CHECK_THROW(output.shape[1] == n_output_dims());
+		CHECK_THROW(params.shape[0] == n_params());
+		CHECK_THROW(output.shape[0] == input.shape[0]);
+		CHECK_THROW(dL_doutput.shape[0] == input.shape[0]);
+
+		// Device
+		auto place = input.place();
+		CHECK_THROW(place == params.place());
+		CHECK_THROW(place == output.place());
+		CHECK_THROW(place == dL_doutput.place());
+
+		cudaStream_t stream = input.stream();
+
+		uint32_t batch_size = static_cast<uint32_t>(input.shape[0]);
+
+		paddle::Tensor dL_dinput;
+		if (input_requires_grad) {
+			dL_dinput = paddle::empty({ batch_size, input.shape[1] }, paddle::DataType::FLOAT32, place);
+		}
+
+		paddle::Tensor dL_dparams;
+		if (params_requires_grad) {
+			dL_dparams = paddle::empty({ n_params() }, c10_param_precision(), place);
+		}
+
+		if (input_requires_grad || params_requires_grad) {
+			m_module->backward(
+				stream,
+				ctx,
+				batch_size,
+				input_requires_grad ? dL_dinput.data<float>() : nullptr,
+				void_data_ptr(dL_doutput),
+				params_requires_grad ? void_data_ptr(dL_dparams) : nullptr,
+				input.data<float>(),
+				void_data_ptr(output),
+				void_data_ptr(params)
+			);
+		}
+
+		return { dL_dinput, dL_dparams };
+	}
+
+	std::tuple<paddle::Tensor, paddle::Tensor, paddle::Tensor> bwd_bwd_input(const tcnn::cpp::Context& ctx, const paddle::Tensor& input, const paddle::Tensor& params, const paddle::Tensor& dL_ddLdinput, const paddle::Tensor& dL_doutput, bool dL_doutput_requires_grad, bool params_requires_grad, bool input_requires_grad) {
+		// from: dL_ddLdinput
+		// to:   dL_ddLdoutput, dL_dparams, dL_dinput
+
+		if (!ctx.ctx) {
+			throw std::runtime_error{"Module::bwd_bwd_input: called with invalid context. fwd likely (mistakenly) ran in inference mode."};
+		}
+
+		CHECK_INPUT(input);
+		CHECK_INPUT(params);
+		CHECK_INPUT(dL_ddLdinput);
+		CHECK_INPUT(dL_doutput);
+
+		// Types
+		CHECK_THROW(input.dtype() == paddle::DataType::FLOAT32);
+		CHECK_THROW(dL_ddLdinput.dtype() == paddle::DataType::FLOAT32);
+		CHECK_THROW(params.dtype() == c10_param_precision());
+		CHECK_THROW(dL_doutput.dtype() == c10_output_precision());
+
+		// Sizes
+		CHECK_THROW(input.shape[1] == n_input_dims());
+		CHECK_THROW(dL_doutput.shape[1] == n_output_dims());
+		CHECK_THROW(dL_ddLdinput.shape[1] == n_input_dims());
+		CHECK_THROW(params.shape[0] == n_params());
+		CHECK_THROW(dL_doutput.shape[0] == input.shape[0]);
+		CHECK_THROW(dL_ddLdinput.shape[0] == input.shape[0]);
+
+		// Device
+		auto place = input.place();
+		CHECK_THROW(place == params.place());
+		CHECK_THROW(place == dL_ddLdinput.place());
+		CHECK_THROW(place == dL_doutput.place());
+
+		cudaStream_t stream = input.stream();
+
+		uint32_t batch_size = static_cast<uint32_t>(input.shape[0]);
+
+		paddle::Tensor dL_ddLdoutput;
+		if (dL_doutput_requires_grad) {
+			dL_ddLdoutput = paddle::zeros({ batch_size, n_output_dims() }, c10_output_precision(), place);
+		}
+
+		paddle::Tensor dL_dparams;
+		if (params_requires_grad) {
+			dL_dparams = paddle::zeros({ n_params() }, c10_param_precision(), place);
+		}
+
+		paddle::Tensor dL_dinput;
+		if (input_requires_grad) {
+			dL_dinput = paddle::zeros({ batch_size, n_input_dims() }, paddle::DataType::FLOAT32, place);
+		}
+
+		if (dL_doutput_requires_grad || params_requires_grad) {
+			m_module->backward_backward_input(
+				stream,
+				ctx,
+				batch_size,
+				dL_ddLdinput.data<float>(),
+				input.data<float>(),
+				(params_requires_grad || input_requires_grad ) ? void_data_ptr(dL_doutput) : nullptr,
+				params_requires_grad ? void_data_ptr(dL_dparams) : nullptr,
+				dL_doutput_requires_grad ? void_data_ptr(dL_ddLdoutput) : nullptr,
+				input_requires_grad ? dL_dinput.data<float>() : nullptr,
+				void_data_ptr(params)
+			);
+		}
+
+		return {dL_ddLdoutput, dL_dparams, dL_dinput};
+	}
+
+	paddle::Tensor initial_params(size_t seed) {
+		paddle::Tensor output = paddle::zeros({ n_params() }, paddle::DataType::FLOAT32, paddle::GPUPlace());
+		m_module->initialize_params(seed, output.data<float>());
+		return output;
+	}
+
+	uint32_t n_input_dims() const {
+		return m_module->n_input_dims();
+	}
+
+	uint32_t n_params() const {
+		return (uint32_t)m_module->n_params();
+	}
+
+	tcnn::cpp::EPrecision param_precision() const {
+		return m_module->param_precision();
+	}
+
+	c10::ScalarType c10_param_precision() const {
+		return paddle_type(param_precision());
+	}
+
+	uint32_t n_output_dims() const {
+		return m_module->n_output_dims();
+	}
+
+	tcnn::cpp::EPrecision output_precision() const {
+		return m_module->output_precision();
+	}
+
+	c10::ScalarType c10_output_precision() const {
+		return paddle_type(output_precision());
+	}
+
+	nlohmann::json hyperparams() const {
+		return m_module->hyperparams();
+	}
+
+	std::string name() const {
+		return m_module->name();
+	}
+
+private:
+	std::unique_ptr<tcnn::cpp::Module> m_module;
+};
+
+#if !defined(TCNN_NO_NETWORKS)
+Module create_network_with_input_encoding(uint32_t n_input_dims, uint32_t n_output_dims, const nlohmann::json& encoding, const nlohmann::json& network) {
+	return Module{tcnn::cpp::create_network_with_input_encoding(n_input_dims, n_output_dims, encoding, network)};
+}
+
+Module create_network(uint32_t n_input_dims, uint32_t n_output_dims, const nlohmann::json& network) {
+	return Module{tcnn::cpp::create_network(n_input_dims, n_output_dims, network)};
+}
+#endif
+
+Module create_encoding(uint32_t n_input_dims, const nlohmann::json& encoding, tcnn::cpp::EPrecision requested_precision) {
+	return Module{tcnn::cpp::create_encoding(n_input_dims, encoding, requested_precision)};
+}
+
+PYBIND11_MODULE(tinycudann, m) {
+	py::enum_<tcnn::cpp::EPrecision>(m, "Precision")
+		.value("Fp32", tcnn::cpp::EPrecision::Fp32)
+		.value("Fp16", tcnn::cpp::EPrecision::Fp16)
+		.export_values()
+		;
+
+	m.def("preferred_precision", &tcnn::cpp::preferred_precision);
+	m.def("batch_size_granularity", &tcnn::cpp::batch_size_granularity);
+	m.def("free_temporary_memory", &tcnn::cpp::free_temporary_memory);
+
+	// Encapsulates an abstract context of an operation
+	// (commonly the forward pass) to be passed on to other
+	// operations (commonly the backward pass).
+	py::class_<tcnn::cpp::Context>(m, "Context");
+
+	// The python bindings expose TCNN's C++ API through
+	// a single "Module" class that can act as the encoding,
+	// the neural network, or a combined encoding + network
+	// under the hood. The bindings don't need to concern
+	// themselves with these implementation details, though.
+	py::class_<Module>(m, "Module")
+		.def("fwd", &Module::fwd)
+		.def("bwd", &Module::bwd)
+		.def("bwd_bwd_input", &Module::bwd_bwd_input)
+		.def("initial_params", &Module::initial_params)
+		.def("n_input_dims", &Module::n_input_dims)
+		.def("n_params", &Module::n_params)
+		.def("param_precision", &Module::param_precision)
+		.def("n_output_dims", &Module::n_output_dims)
+		.def("output_precision", &Module::output_precision)
+		.def("hyperparams", &Module::hyperparams)
+		.def("name", &Module::name)
+		;
+
+#if !defined(TCNN_NO_NETWORKS)
+	m.def("create_network_with_input_encoding", &create_network_with_input_encoding);
+	m.def("create_network", &create_network);
+#endif
+
+	m.def("create_encoding", &create_encoding);
+}
---
+++ bindings/paddle/tinycudann/modules.py
@@ -0,0 +1,261 @@
+# Copyright (c) 2020-2021, NVIDIA CORPORATION. All rights reserved.
+#
+# NVIDIA CORPORATION and its licensors retain all intellectual property
+# and proprietary rights in and to this software, related documentation
+# and any modifications thereto. Any use, reproduction, disclosure or
+# distribution of this software and related documentation without an express
+# license agreement from NVIDIA CORPORATION is strictly prohibited.
+
+import paddle
+from paddle.nn.initializer import Assign
+from tinycudann_bindings import _C
+
+
+def _paddle_precision(tcnn_precision):
+	if tcnn_precision == _C.Precision.Fp16:
+		return "float16"
+	elif tcnn_precision == _C.Precision.Fp32:
+		return "float32"
+	else:
+		raise ValueError(f"Unknown precision {tcnn_precision}")
+
+
+def free_temporary_memory():
+	_C.free_temporary_memory()
+
+
+class _module_function(paddle.autograd.PyLayer):
+	@staticmethod
+	def forward(ctx, native_tcnn_module, input, params, loss_scale):
+		native_ctx, output = native_tcnn_module.fwd(input, params, input_requires_grad=True, params_requires_grad=True)
+		ctx.save_for_backward(input, params, output)
+		ctx.native_tcnn_module = native_tcnn_module
+		ctx.native_ctx = native_ctx
+		ctx.loss_scale = loss_scale
+
+		return output
+
+	@staticmethod
+	def backward(ctx, doutput):
+		if doutput is None:
+			return None, None, None, None
+
+		input, params, output = ctx.saved_tensors
+		input_grad, weight_grad = _module_function_backward.apply(ctx, doutput, input, params, output)
+
+		return None, input_grad, weight_grad, None
+
+
+class _module_function_backward(paddle.autograd.PyLayer):
+	@staticmethod
+	def forward(ctx, ctx_fwd, doutput, input, params, output):
+		ctx.ctx_fwd = ctx_fwd
+		ctx.save_for_backward(input, params, doutput)
+		with paddle.no_grad():
+			scaled_grad = doutput * ctx_fwd.loss_scale
+			input_grad, weight_grad = ctx_fwd.native_tcnn_module.bwd(ctx_fwd.native_ctx, input, params, output,
+																	 scaled_grad, input_requires_grad=False,
+																	 params_requires_grad=False)
+			input_grad = None if input_grad is None else (input_grad / ctx_fwd.loss_scale)
+			weight_grad = None if weight_grad is None else (weight_grad / ctx_fwd.loss_scale)
+		return input_grad, weight_grad
+
+	@staticmethod
+	def backward(ctx, dinput_grad, dweight_grad):
+		# NOTE: currently support:
+		#       ✓   d(dL_dinput)_d(dL_doutput)  doutput_grad
+		#       ✓   d(dL_dinput)_d(params)      weight_grad
+		#       ✓   d(dL_dinput)_d(input)       input_grad
+		#       x   d(dL_dparam)_d(...)
+		input, params, doutput = ctx.saved_tensors
+		# assert dweight_grad is None, "currently do not support 2nd-order gradients from gradient of grid"
+		with paddle.set_grad_enabled(True):
+			# NOTE: preserves requires_grad info (this function is in no_grad() context by default when invoking loss.backward())
+			doutput = doutput * ctx.ctx_fwd.loss_scale
+		with paddle.no_grad():
+			doutput_grad, weight_grad, input_grad = ctx.ctx_fwd.native_tcnn_module.bwd_bwd_input(
+				ctx.ctx_fwd.native_ctx,
+				input,
+				params,
+				dinput_grad,
+				doutput,
+				dL_doutput_requires_grad=False,
+				params_requires_grad=False,
+				input_requires_grad=False
+			)
+			# NOTE: be cautious when multiplying and dividing loss_scale
+			#       doutput_grad uses dinput_grad
+			#       weight_grad  uses dinput_grad * doutput
+			#       input_grad   uses dinput_grad * doutput
+			weight_grad = None if weight_grad is None else (weight_grad / ctx.ctx_fwd.loss_scale)
+			input_grad = None if input_grad is None else (input_grad / ctx.ctx_fwd.loss_scale)
+
+		# ctx_fwd,   doutput,      input,      params,      output
+		return None, doutput_grad, input_grad, weight_grad, None
+
+
+class Module(paddle.nn.Layer):
+	def __init__(self, seed=1337):
+		super(Module, self).__init__()
+
+		self.native_tcnn_module = self._native_tcnn_module()
+		self.dtype = _paddle_precision(self.native_tcnn_module.param_precision())
+
+		self.seed = seed
+		initial_params = self.native_tcnn_module.initial_params(seed)
+		self.params = paddle.create_parameter(initial_params.shape, initial_params.dtype,
+											  default_initializer=Assign(initial_params))
+
+		self.loss_scale = 128.0 if self.native_tcnn_module.param_precision() == _C.Precision.Fp16 else 1.0
+
+	def forward(self, x):
+		batch_size = x.shape[0]
+		batch_size_granularity = int(_C.batch_size_granularity())
+		padded_batch_size = (batch_size + batch_size_granularity - 1) // batch_size_granularity * batch_size_granularity
+
+		x_padded = x if batch_size == padded_batch_size else paddle.nn.functional.pad(x, [0, 0, 0, padded_batch_size - batch_size])
+		output = _module_function.apply(
+			self.native_tcnn_module,
+			x_padded,
+			self.params.astype(_paddle_precision(self.native_tcnn_module.param_precision())),
+			self.loss_scale
+		)
+		return output[:batch_size, :self.n_output_dims]
+
+	def __getstate__(self):
+		"""Return state values to be pickled."""
+		state = self.__dict__.copy()
+		# Avoid pickling native objects
+		del state["native_tcnn_module"]
+		return state
+
+	def __setstate__(self, state):
+		self.__dict__.update(state)
+		# Reconstruct native entries
+		self.native_tcnn_module = self._native_tcnn_module()
+
+	def extra_repr(self):
+		return f"n_input_dims={self.n_input_dims}, n_output_dims={self.n_output_dims}, seed={self.seed}, dtype={self.dtype}, hyperparams={self.native_tcnn_module.hyperparams()}"
+
+
+class NetworkWithInputEncoding(Module):
+	"""
+	Input encoding, followed by a neural network.
+
+	This module is more efficient than invoking individual `Encoding`
+	and `Network` modules in sequence.
+
+	Takes a `torch.float` input tensor of shape `[:, n_input_dims]` and maps
+	it to a tensor of shape `[:, n_output_dims]`.
+
+	The output tensor can be either of type `torch.float` or `torch.half`,
+	depending on which performs better on the system.
+
+	Parameters
+	----------
+	n_input_dims : `int`
+		Determines the shape of input tensors as `[:, n_input_dims]`
+	n_output_dims : `int`
+		Determines the shape of output tensors as `[:, n_output_dims]`
+	encoding_config: `dict`
+		Configures the encoding. Possible configurations are documented at
+		https://github.com/NVlabs/tiny-cuda-nn/blob/master/DOCUMENTATION.md
+	network_config: `dict`
+		Configures the neural network. Possible configurations are documented at
+		https://github.com/NVlabs/tiny-cuda-nn/blob/master/DOCUMENTATION.md
+	seed: `int`
+		Seed for pseudorandom parameter initialization
+	"""
+
+	def __init__(self, n_input_dims, n_output_dims, encoding_config, network_config, seed=1337):
+		self.n_input_dims = n_input_dims
+		self.n_output_dims = n_output_dims
+		self.encoding_config = encoding_config
+		self.network_config = network_config
+
+		super(NetworkWithInputEncoding, self).__init__(seed=seed)
+
+	def _native_tcnn_module(self):
+		return _C.create_network_with_input_encoding(self.n_input_dims, self.n_output_dims, self.encoding_config,
+													 self.network_config)
+
+
+class Network(Module):
+	"""
+	Neural network.
+
+	Takes a `torch.float` input tensor of shape `[:, n_input_dims]` and maps
+	it to a tensor of shape `[:, n_output_dims]`.
+
+	The output tensor can be either of type `torch.float` or `torch.half`,
+	depending on which performs better on the system.
+
+	Parameters
+	----------
+	n_input_dims : `int`
+		Determines the shape of input tensors as `[:, n_input_dims]`
+	n_output_dims : `int`
+		Determines the shape of output tensors as `[:, n_output_dims]`
+	network_config: `dict`
+		Configures the neural network. Possible configurations are documented at
+		https://github.com/NVlabs/tiny-cuda-nn/blob/master/DOCUMENTATION.md
+	seed: `int`
+		Seed for pseudorandom parameter initialization
+	"""
+
+	def __init__(self, n_input_dims, n_output_dims, network_config, seed=1337):
+		self.n_input_dims = n_input_dims
+		self.n_output_dims = n_output_dims
+		self.network_config = network_config
+
+		super(Network, self).__init__(seed=seed)
+
+	def _native_tcnn_module(self):
+		return _C.create_network(self.n_input_dims, self.n_output_dims, self.network_config)
+
+
+class Encoding(Module):
+	"""
+	Input encoding to a neural network.
+
+	Takes a `torch.float` input tensor of shape `[:, n_input_dims]` and maps
+	it to a `dtype` tensor of shape `[:, self.n_output_dims]`, where
+	`self.n_output_dims` depends on `n_input_dims` and the configuration
+	`encoding_config`.
+
+	Parameters
+	----------
+	n_input_dims : `int`
+		Determines the shape of input tensors as `[:, n_input_dims]`
+	encoding_config: `dict`
+		Configures the encoding. Possible configurations are documented at
+		https://github.com/NVlabs/tiny-cuda-nn/blob/master/DOCUMENTATION.md
+	seed: `int`
+		Seed for pseudorandom parameter initialization
+	dtype: `torch.dtype`
+		Precision of the output tensor and internal parameters. A value
+		of `None` corresponds to the optimally performing precision,
+		which is `torch.half` on most systems. A value of `torch.float`
+		may yield higher numerical accuracy, but is generally slower.
+		A value of `torch.half` may not be supported on all systems.
+	"""
+
+	def __init__(self, n_input_dims, encoding_config, seed=1337, dtype=None):
+		self.n_input_dims = n_input_dims
+		self.encoding_config = encoding_config
+		if dtype is None:
+			self.precision = _C.preferred_precision()
+		else:
+			if dtype == "float32":
+				self.precision = _C.Precision.Fp32
+			elif dtype == "float16":
+				self.precision = _C.Precision.Fp16
+			else:
+				raise ValueError(f"Encoding only supports fp32 or fp16 precision, but got {dtype}")
+
+		super(Encoding, self).__init__(seed=seed)
+
+		self.n_output_dims = self.native_tcnn_module.n_output_dims()
+
+	def _native_tcnn_module(self):
+		return _C.create_encoding(self.n_input_dims, self.encoding_config, self.precision)
